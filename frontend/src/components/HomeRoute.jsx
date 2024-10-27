443aa7942766f18c6dd0f275c1f8a960e520f71b
MichaelJGryzz <michaeljgryschuk@gmail.com>
2024-10-27 17:47:40 -0400

    Reorder imports in attempt to satisfy AI feedback

--- frontend/src/components/HomeRoute.jsx
+++ frontend/src/components/HomeRoute.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import PhotoList from './PhotoList';
 import TopNavigation from './TopNavigationBar';
+import PhotoList from './PhotoList';
 
 import "../styles/HomeRoute.scss";
 
